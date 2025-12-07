44d543036346aedefffdafb29c5660486b1a9545
Minhanab <nabaminhaa@gmail.com>
2025-12-07 03:16:42 -0500

    Added BlackJackGame copying all methods into this class for play methods to run in the main application. is also includes alternative rounds and declarewinner methods. Please check for errors when testing and add comments to all code

--- BlackJackProject-Deliverable2_fixes/BlackJackProject-Deliverable2_fixes/BlackJackProject-Deliverable2_fixes/BlackJackDeliverable2/src/main/java/com/mycompany/blackjackdeliverable2/BlackJackDeliverable2.java
+++ BlackJackProject-Deliverable2_fixes/BlackJackProject-Deliverable2_fixes/BlackJackProject-Deliverable2_fixes/BlackJackDeliverable2/src/main/java/com/mycompany/blackjackdeliverable2/BlackJackDeliverable2.java
@@ -6,8 +6,6 @@ package com.mycompany.blackjackdeliverable2;
 
 import java.util.Scanner;
 
-
-
 /**
  *
  * @author spmar
@@ -20,94 +18,11 @@ public class BlackJackDeliverable2 {
     public static void main(String[] args) {
         // TODO code application logic here
         //Ask Professor tomorrow if this is needed for application purposes
-        LoginManager loginManager = new LoginManager();
-        String username = loginManager.displayLoginMenu();
-        
-        //initializing deck and player and shuffling the deck, and then as 
-        //the round starts, the player takes 2 cards from the deck
-        BlackJackDeck deck = BlackJackDeck.getInstance(); //creating a deck
-        deck.shuffle();
-        System.out.println("\nDeck Shuffled");
-        
-        
-        //The Creation of Player and Dealer
-        BlackJackPlayer player = new BlackJackPlayer(username);
-        BlackJackDealer dealer = new BlackJackDealer("Dealer");
-        
-        
-        
-        System.out.println("\n== BLACKJACK ==");
-        System.out.println(username + "'s hand:" + player.getHand().getCards() + " = " 
-                + player.getHand().getScore());
-        System.out.println("Dealer showing: " + dealer.getHand().getCards().get(0));
-        
-        //Before any turn using a while loop if player/dealer hit 21 or higher busted, if player or dealer hits 21 , then either player wins or loses
-        if (player.getHand().getScore()== 21 && dealer.getHand().getScore() == 21){
-            System.out.println("Its a draw!");
-        }
-        else if (player.getHand().getScore()== 21) {
-            System.out.println(username + " has blackjack! " + username + " wins!");
-            return;
-        }
-        else if (dealer.getHand().getScore()== 21) {
-            System.out.println("Dealer has blackjack! Dealer wins!");
-            return;
-        }
-        //Player's turn
-        
-        Scanner scanner = new Scanner(System.in);
-        while(true) {
-            System.out.print("\n Hit or Stand? (Hit/Stand): ");
-            String choice = scanner.nextLine().trim().toLowerCase();
-            
-            if(choice.equals("hit")) {
-                //error because this is instially creating a brand new deck due to boolean logic
-                player.getHand().drawCard(deck);
-                System.out.println(username + "'s hand: " + player.getHand() .getCards()
-                + player.getHand().getScore());
-                if(player.getHand().getScore() > 21) {
 
-                //we need to make a clause that make it show your high score/low score when you have aces for clarity
-                    if(player.getHand().getScore() > 21) {
-                        System.out.println(username + " busted! Dealer wins!");
-                        return;
-                    }
-                }
-            }
-            else if (choice.equals("stand")) {
-                break;
-            }
-            else{
-                System.out.println("Invalid input! Please enter 'Hit' or 'Stand'");
-            }
-        }
-        
-        //Dealer's turn
-        // Same logic as player
-        
-        System.out.println("\nDealer's turn...");
-        dealer.play();
-        System.out.println("Dealer hand: " + dealer.getHand().getCards()+ " = " + dealer.getHand().getScore());
-        
-        int playerScore = player.getHand().getScore();
-        int dealerScore = dealer.getHand().getScore();
-        
-        if (dealerScore > 21) {
-            System.out.println("Dealer busted ! " + username + " wins!");
-            
-        }else if (playerScore > dealerScore) {
-            System.out.println(username + " wins!");
-        }else if (dealerScore > playerScore) {
-            System.out.println("Dealer wins!");
-        }else {
-            System.out.println("Its a draw!");
-        }
-        //Duplication score and getCard
-//        Hand playerHand = new Hand(deck);
-//        System.out.println("Player Starting Hand: " + playerHand.getCards());
-//        System.out.println("Player hand value: " + playerHand.getScore());
-//        System.out.println("Deck after Player drew his cards" + deck.getCards());
+        BlackJackGame game = new BlackJackGame("BlackJack");
+
+        game.play();
 
     }
-    
+
 }
---
+++ BlackJackProject-Deliverable2_fixes/BlackJackProject-Deliverable2_fixes/BlackJackProject-Deliverable2_fixes/BlackJackDeliverable2/src/main/java/com/mycompany/blackjackdeliverable2/BlackJackGame.java
@@ -0,0 +1,172 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.blackjackdeliverable2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Naba Minhas
+ * @author Sebastian
+ */
+public class BlackJackGame extends Game {
+
+    private int playerPoints = 0;
+    private int dealerPoints = 0;
+
+    public BlackJackGame(String name) {
+        super(name);
+    }
+
+    @Override
+    public void play() {
+
+        LoginManager loginManager = new LoginManager();
+        String username = loginManager.displayLoginMenu();
+
+        BlackJackDeck deck = BlackJackDeck.getInstance(); //creating a deck
+        deck.shuffle();
+        System.out.println("\nDeck Shuffled");
+
+        //The Creation of Player and Dealer
+        BlackJackPlayer player = new BlackJackPlayer(username);
+        BlackJackDealer dealer = new BlackJackDealer("Dealer");
+
+        System.out.println("\n== BLACKJACK ==");
+        System.out.println(username + "'s hand:" + player.getHand().getCards() + " = "
+                + player.getHand().getScore());
+        System.out.println("Dealer showing: " + dealer.getHand().getCards().get(0) + " , Hidden Card");
+
+        //Before any turn using a while loop if player/dealer hit 21
+        //or higher busted, if player or dealer hits 21 , then either player wins or loses and also uses the rounds points 
+        if (player.getHand().getScore() == 21 && dealer.getHand().getScore() == 21) {
+            System.out.println("Its a draw!");
+            System.out.println("Player Points: " + playerPoints);
+            System.out.println("Dealer Points: " + dealerPoints);
+            declareWinner();
+            return;
+        } else if (player.getHand().getScore() == 21) {
+            System.out.println(username + " has blackjack! " + username + " wins!");
+            playerPoints++;
+            System.out.println("Player Points: " + playerPoints);
+            System.out.println("Dealer Points: " + dealerPoints);
+            declareWinner();
+            return;
+        } else if (dealer.getHand().getScore() == 21) {
+            System.out.println("Dealer has blackjack! Dealer wins!");
+             dealerPoints++;
+            System.out.println("Player Points: " + playerPoints);
+            System.out.println("Dealer Points: " + dealerPoints);
+            declareWinner();   
+            return;
+        }
+        //Player's turn
+
+        Scanner scanner = new Scanner(System.in);
+        boolean playerStands = false;// track if the player chosen to stand but as default its not determined
+
+        while (true) {
+            // only asks if the player hasn't stood or hit yet
+            if (!playerStands) {
+                System.out.print("\n Hit or Stand? (Hit/Stand): ");
+                String choice = scanner.nextLine().trim().toLowerCase();
+
+                if (choice.equals("hit")) {
+                    player.getHand().drawCard(deck);
+                    System.out.println(username + "'s hand: " + player.getHand().getCards()
+                            + player.getHand().getScore());
+                    if (player.getHand().getScore() > 21) {
+                        System.out.println(username + " busted! Dealer wins!");
+                        dealerPoints++;
+                        System.out.println("Player Points: " + playerPoints);
+                        System.out.println("Dealer Points: " + dealerPoints);
+                    declareWinner();    
+                        return;
+                    }
+                } else if (choice.equals("stand")) {
+                    playerStands = true;
+                } else {
+                    System.out.println("Invalid input! Please enter 'Hit' or 'Stand'");
+                }
+            }
+
+            //Player stand and round moves to dealer next
+            if (dealer.getHand().getScore() < 17) {
+                dealer.getHand().drawCard(deck);
+                System.out.println("Dealer hits. Dealer hand:" + dealer.getHand().getCards() + " = " + dealer.getHand().getScore());
+                if (dealer.getHand().getScore() > 21) {
+                    System.out.println("Dealer busted! " + username + " wins!");
+                    playerPoints++;  // Add point to dealer
+                    System.out.println("Player Points: " + playerPoints);
+                    System.out.println("Dealer Points: " + dealerPoints);
+                    declareWinner();
+                    return;
+                }
+            } else if (playerStands) {
+                //exits the loops if player has finished their stood and dealer has reached 17+ cards
+                break;
+            }
+        }
+           //Dealer finishes their hand until they reach 17+ cards and the winner will be determined
+            while(dealer.getHand().getScore() < 17)  {
+                dealer.getHand().drawCard(deck);
+                System.out.println("Dealer hits. Dealer hand:" + dealer.getHand().getCards() + " = " + dealer.getHand().getScore());
+                if (dealer.getHand().getScore() > 21) {
+                    System.out.println("Dealer busted! " + username + " wins!");
+                    playerPoints++;
+                    System.out.println("Player Points: " + playerPoints);
+                    System.out.println("Dealer Points: " + dealerPoints);
+                    declareWinner(); 
+                    return;
+                }
+            }
+                  
+
+         
+        //Determine the winner
+         roundWinner(player, dealer, username);
+         System.out.println("Player Points: " + playerPoints);
+         System.out.println("Dealer Points: " + dealerPoints);
+         declareWinner();
+         
+       
+
+    }
+// decalares the winner
+    @Override
+    public void declareWinner() {
+        if (dealerPoints > playerPoints) {
+            System.out.println("Dealer wins overall!");
+        } else if (playerPoints > dealerPoints) {
+            System.out.println("Player wins overall!");
+
+        } else {
+            System.out.println("Its a draw. Both win overall !");
+        }
+
+    }
+//tracks number of rounds won 
+    public void roundWinner(BlackJackPlayer player, BlackJackDealer dealer, String username) {
+
+        int playerScore = player.getHand().getScore();
+        int dealerScore = dealer.getHand().getScore();
+
+        if (dealerScore > 21) {
+            System.out.println("Dealer busted ! " + username + " wins this round!");
+            playerPoints++;
+
+        } else if (playerScore > dealerScore) {
+            System.out.println(username + " wins this round!");
+            playerPoints++;
+        } else if (dealerScore > playerScore) {
+            System.out.println("Dealer wins this round!");
+            dealerPoints++;
+        } else {
+            System.out.println("Its a draw for both this round!");
+        }
+
+    }
+
+}
